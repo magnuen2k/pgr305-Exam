f26b81e5843808d50a49d0b22fc0992c0657eaa7
magnuen2k <maen030@student.kristiania.no>
2021-11-16 13:47:05 +0100

    add filter option to players, should refactor to own component

--- client/src/components/Player/PlayerList.tsx
+++ client/src/components/Player/PlayerList.tsx
@@ -1,5 +1,5 @@
-import React, { FC, useContext } from "react";
-import { Row, Col, Container } from "react-bootstrap";
+import React, { FC, useContext, useEffect, useState } from "react";
+import { Row, Col, Container, Form } from "react-bootstrap";
 import { PlayerContext } from "../../contexts/PlayerContext";
 import { IPlayer } from "../../interfaces/IPlayer";
 import { PlayerContextType } from "../../types/PlayerContextType";
@@ -7,9 +7,13 @@ import PlayerItem from "./PlayerItem";
 
 const PlayerList: FC = () => {
   const { players } = useContext(PlayerContext) as PlayerContextType;
+  const [allPlayers, setAllPlayers] = useState<IPlayer[]>([]);
 
   const createPlayerList = () => {
-    return players.map((player: IPlayer, key: number) => {
+    // Display either full list of players or filtered copy
+    const list = allPlayers.length > 0 ? allPlayers : players;
+
+    return list.map((player: IPlayer, key: number) => {
       return (
         <Col md={6} lg={4} xl={3} className="mt-5">
           <PlayerItem
@@ -27,8 +31,28 @@ const PlayerList: FC = () => {
     });
   };
 
+  const handleChange = (e: any) => {
+    if (e.target.value === "default") {
+      setAllPlayers(players);
+    } else {
+      setAllPlayers(
+        players.filter((player: IPlayer) => player.position === e.target.value)
+      );
+    }
+
+    console.log(allPlayers);
+  };
+
   return (
     <Container>
+      <Form.Select onChange={handleChange}>
+        <option value="default">Filter by position</option>
+        <option value="Keeper">Keeper</option>
+        <option value="Defender">Defender</option>
+        <option value="Midfielder">Midfielder</option>
+        <option value="Forward">Forward</option>
+      </Form.Select>
+
       <Row>{createPlayerList()}</Row>
     </Container>
   );
