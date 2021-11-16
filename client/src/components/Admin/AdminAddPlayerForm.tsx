f372f7cacf9a0105d1a36e4c25af25fdcc17f772
magnuen2k <maen030@student.kristiania.no>
2021-11-16 18:22:36 +0100

    add image uploading when adding a player

--- client/src/components/Admin/AdminAddPlayerForm.tsx
+++ client/src/components/Admin/AdminAddPlayerForm.tsx
@@ -1,5 +1,6 @@
+import axios from "axios";
 import React, { useState } from "react";
-import { Button, FormControl, InputGroup } from "react-bootstrap";
+import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
 import { IPlayer } from "../../interfaces/IPlayer";
 
 const AdminAddPlayerForm = () => {
@@ -13,8 +14,35 @@ const AdminAddPlayerForm = () => {
   };
 
   const [player, setPlayer] = useState<IPlayer>(initialState);
+  const [file, setFile] = useState<File>();
 
-  const addPlayer = () => {
+  const addPlayer = async () => {
+    // Upload image to server, if successful, add player to database
+    let data = new FormData();
+
+    if (file) {
+      data.append("file", file);
+    }
+
+    let res;
+
+    try {
+      res = await axios({
+        method: "POST",
+        url: "https://localhost:5001/ImageUpload/SaveImage",
+        data: data,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+
+    if (res?.status === 201) {
+      // Add player to database
+    }
+
     console.log(player);
 
     // Clear input form
@@ -23,33 +51,51 @@ const AdminAddPlayerForm = () => {
 
   return (
     <div className="mb-2 mt-5">
-      <InputGroup>
-        <FormControl
-          placeholder="Name"
-          value={player.name}
-          onChange={(e) => setPlayer({ ...player, name: e.target.value })}
-        />
-        <FormControl
-          placeholder="Nationality"
-          value={player.nationality}
-          onChange={(e) =>
-            setPlayer({ ...player, nationality: e.target.value })
-          }
-        />
-        <FormControl
-          placeholder="Nationality"
-          type="date"
-          onChange={(e) =>
-            setPlayer({ ...player, dateOfBirth: new Date(e.target.value) })
-          }
-        />
-        <FormControl
-          placeholder="Position"
-          value={player.position}
-          onChange={(e) => setPlayer({ ...player, position: e.target.value })}
-        />
+      <Form>
+        <Form.Group>
+          <FormControl
+            placeholder="Name"
+            value={player.name}
+            onChange={(e) => setPlayer({ ...player, name: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group>
+          <FormControl
+            placeholder="Nationality"
+            value={player.nationality}
+            onChange={(e) =>
+              setPlayer({ ...player, nationality: e.target.value })
+            }
+          />
+        </Form.Group>
+        <Form.Group>
+          <FormControl
+            placeholder="Nationality"
+            type="date"
+            onChange={(e) =>
+              setPlayer({ ...player, dateOfBirth: new Date(e.target.value) })
+            }
+          />
+        </Form.Group>
+        <Form.Group>
+          <FormControl
+            placeholder="Position"
+            value={player.position}
+            onChange={(e) => setPlayer({ ...player, position: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Upload an image</Form.Label>
+          <Form.Control
+            type="file"
+            onChange={(e: any) => {
+              setPlayer({ ...player, image: e.target.files[0].name });
+              setFile(e.target.files[0]);
+            }}
+          />
+        </Form.Group>
         <Button onClick={addPlayer}>Add player</Button>
-      </InputGroup>
+      </Form>
     </div>
   );
 };
