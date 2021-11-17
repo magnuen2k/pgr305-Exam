b973ec645081d167f7ee6082c3a3a53e6a3fcdee
Kaikka <kaileeto@gmail.com>
2021-11-17 18:31:00 +0100

    removed link and added all info in staff

--- client/src/components/Staff/StaffItem.tsx
+++ client/src/components/Staff/StaffItem.tsx
@@ -3,6 +3,7 @@ import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { IStaff } from "../../interfaces/IStaff";
 import { API_URL } from "../../utils/Constants";
+import { getAge, getSimpleBirthOfDate } from "../../utils/GetAge";
 
 const StaffItem: FC<IStaff> = ({
   id,
@@ -14,18 +15,27 @@ const StaffItem: FC<IStaff> = ({
   role,
 }) => {
   return (
-    <Link to={`/staff-details/${id}`}>
-      <Card className="card-container Staff-card">
-        <div className="card-image-container img-responsive">
-          <Card.Img
-            variant="top"
-            src={`${API_URL}/images/${image}`}
-            className="img-fluid"
-          />
-        </div>
-        <Card.Title className="Staff-card-name">{name}</Card.Title>
-      </Card>
-    </Link>
+    <Card className="card-container Staff-card">
+      <div className="card-image-container img-responsive">
+        <Card.Img
+          variant="top"
+          src={`${API_URL}/images/${image}`}
+          className="img-fluid"
+        />
+      </div>
+      {/*<Card.Title className="Staff-card-name">{name}</Card.Title>*/}
+      <Card.Body>
+        <Card.Text>Name: {name}</Card.Text>
+        <Card.Text>Club: {club}</Card.Text>
+        <Card.Text>
+          Date of birth: {getSimpleBirthOfDate(dateOfBirth)} (age{" "}
+          {getAge(dateOfBirth)})
+        </Card.Text>
+        <Card.Text>Place of birth: {nationality}</Card.Text>
+
+        <Card.Text>Role: {role}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
