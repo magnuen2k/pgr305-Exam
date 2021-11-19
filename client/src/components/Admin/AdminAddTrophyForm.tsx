c1fb4e790556d65d4c47c1d8f60bcb8565c1bed3
magnuen2k <maen030@student.kristiania.no>
2021-11-19 23:02:28 +0100

    can now add multiple years won when adding a trophy

--- client/src/components/Admin/AdminAddTrophyForm.tsx
+++ client/src/components/Admin/AdminAddTrophyForm.tsx
@@ -1,5 +1,12 @@
 import React, { FC, useContext, useState } from "react";
-import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Form,
+  FormControl,
+  InputGroup,
+  Modal,
+} from "react-bootstrap";
 import { TrophyContext } from "../../contexts/TrophyContext";
 import { ITrophy } from "../../interfaces/ITrophy";
 import { IResponse } from "../../interfaces/IResponse";
@@ -7,12 +14,13 @@ import { TrophyContextType } from "../../types/TrophyContextType";
 import { handleError, handleImageUpload } from "../../utils";
 import Loading from "../shared/Loading";
 import ResponseView from "../shared/ResponseView";
+import styled from "styled-components";
 
 const AdminAddTrophyForm = () => {
   const initialState = {
     tournamentName: "",
     image: "default.jpeg",
-    yearsWon: [1994, 1995],
+    yearsWon: [],
   };
 
   const [trophy, setTrophy] = useState<ITrophy>(initialState);
@@ -20,6 +28,11 @@ const AdminAddTrophyForm = () => {
   const [response, setResponse] = useState<IResponse>();
   const { addTrophy } = useContext(TrophyContext) as TrophyContextType;
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [year, setYear] = useState<string>("");
+  const [isPopup, setIsPopup] = useState<boolean>(false);
+
+  const handleClose = () => setIsPopup(false);
+  const handleShow = () => setIsPopup(true);
 
   // Upload image to server, if successful, add trophy to database
   const addNewTrophy = async () => {
@@ -51,6 +64,15 @@ const AdminAddTrophyForm = () => {
     setTrophy(initialState);
   };
 
+  const addYearWon = () => {
+    if (year.length === 4) {
+      setTrophy({ ...trophy, yearsWon: [...trophy.yearsWon, parseInt(year)] });
+      setYear("");
+    } else {
+      setIsPopup(true);
+    }
+  };
+
   return (
     <div className="mb-2 mt-5">
       <Form>
@@ -63,15 +85,21 @@ const AdminAddTrophyForm = () => {
             }
           />
         </Form.Group>
-        {/*        <Form.Group> trenger å kunne deale med array greier her
+        <Form.Group>
           <FormControl
-            placeholder="Nationality"
-            value={trophy.nationality}
-            onChange={(e) =>
-              setTrophy({ ...trophy, nationality: e.target.value })
-            }
+            type="number"
+            placeholder="Years won"
+            value={year}
+            onChange={(e: any) => setYear(e.target.value)}
           />
-        </Form.Group>*/}
+          <Button onClick={addYearWon}>Add year won</Button>
+          <div>
+            {trophy.yearsWon?.map((y, key) => {
+              if (key === 0) return <span key={key}>{y}</span>;
+              return <span key={key}>, {y}</span>;
+            })}
+          </div>
+        </Form.Group>
         <Form.Group controlId="formFile" className="mb-3">
           <Form.Control
             type="file"
@@ -86,6 +114,13 @@ const AdminAddTrophyForm = () => {
           Add trophy
         </Button>
       </Form>
+      <StyledModal show={isPopup} onHide={handleClose}>
+        <Modal.Header closeButton onClick={handleClose}></Modal.Header>
+
+        <Modal.Body>
+          <p>You have to provide a year with 4 numbers. Got {year.length}.</p>
+        </Modal.Body>
+      </StyledModal>
       {isLoading && <Loading />}
       {response && (
         <ResponseView
@@ -96,5 +131,9 @@ const AdminAddTrophyForm = () => {
     </div>
   );
 };
+
+const StyledModal = styled(Modal)`
+  margin-top: 30vh;
+`;
 
 export default AdminAddTrophyForm;
