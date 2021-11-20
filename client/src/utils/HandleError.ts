70e00c9ea8ee5f9f3dca5fba71c0711f1749f03a
Kaikka <kaileeto@gmail.com>
2021-11-20 21:21:33 +0100

    comments and removed unused imports

--- client/src/utils/HandleError.ts
+++ client/src/utils/HandleError.ts
@@ -1,11 +1,13 @@
 import { IResponse } from "../interfaces/IResponse";
 
+// Sets response message and updates loading state
 export const handleError = (
   e: any,
   setIsLoading: (val: boolean) => void,
   setResponse: (val: IResponse) => void
 ) => {
   setIsLoading(false);
+  // If response exists, set response state to content given by error. Else assume 'Network error'
   if (e.response) {
     setResponse({
       message: e.message,
